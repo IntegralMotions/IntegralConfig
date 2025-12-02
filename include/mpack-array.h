d8842acdcd04b43335d370bdfe70e8b2f0d5b983
JosWigchert <joswigchert@gmail.com>
2025-12-02 01:16:20 +0100

    format file

--- include/mpack-array.h
+++ include/mpack-array.h
@@ -3,20 +3,36 @@
 #include <cstddef>
 
 struct MPackArrayBase {
-  size_t size = 0;
-  void *p = nullptr;
+    size_t size = 0;
+    void* p = nullptr;
 };
 
 template <typename T> struct MPackArray : MPackArrayBase {
-  operator T *() { return static_cast<T *>(p); }
-  operator const T *() const { return static_cast<const T *>(p); }
+    operator T*() {
+        return static_cast<T*>(p);
+    }
+    operator const T*() const {
+        return static_cast<const T*>(p);
+    }
 
-  T &operator[](size_t index) { return static_cast<T *>(p)[index]; }
-  const T &operator[](size_t index) const { return static_cast<const T *>(p)[index]; }
+    T& operator[](size_t index) {
+        return static_cast<T*>(p)[index];
+    }
+    const T& operator[](size_t index) const {
+        return static_cast<const T*>(p)[index];
+    }
 
-  T *begin() { return static_cast<T *>(p); }
-  T *end() { return static_cast<T *>(p) + size; }
+    T* begin() {
+        return static_cast<T*>(p);
+    }
+    T* end() {
+        return static_cast<T*>(p) + size;
+    }
 
-  const T *begin() const { return static_cast<const T *>(p); }
-  const T *end() const { return static_cast<const T *>(p) + size; }
+    const T* begin() const {
+        return static_cast<const T*>(p);
+    }
+    const T* end() const {
+        return static_cast<const T*>(p) + size;
+    }
 };
